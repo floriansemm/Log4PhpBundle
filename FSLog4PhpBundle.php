54dc1e76482ed8d19cad0b46e357fed284280efe
Florian Semm <florian.semm@gmx.de>
2011-09-18 12:18:24 +0200

    Bundle init

---
+++ FSLog4PhpBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace FS\Log4PhpBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Description of FSLog4PhpBundle
+ *
+ * @author Florian
+ */
+class FSLog4PhpBundle extends Bundle {
+    
+}
+
+?>
